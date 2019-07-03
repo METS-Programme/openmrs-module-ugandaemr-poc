35f63ed82b8d269abbd1ec13328a94c2432e90e2
Lubwama Samuel <lubwamasamuel3@gmail.com>
2019-07-03 16:57:47 +0300

    Initial Module Commit (#1)

---
+++ api/src/main/java/org/openmrs/module/ugandaemrpoc/UgandaEMRPOCActivator.java
@@ -0,0 +1,35 @@
+package org.openmrs.module.ugandaemrpoc;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.BaseModuleActivator;
+import org.openmrs.module.aijar.activator.HtmlFormsInitializer;
+import org.openmrs.module.aijar.activator.Initializer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UgandaEMRPOCActivator extends BaseModuleActivator {
+	
+	private Log log = LogFactory.getLog(getClass());
+	
+	public UgandaEMRPOCActivator() {
+	}
+	
+	public void started() {
+		for (Initializer initializer : getInitializers()) {
+			initializer.started();
+		}
+		log.info("Started UgandaEMRPOC");
+	}
+	
+	public void shutdown() {
+		log.info("Shutdown UgandaEMRPOC");
+	}
+	
+	private List<Initializer> getInitializers() {
+		List<Initializer> l = new ArrayList<Initializer>();
+		l.add(new HtmlFormsInitializer(UgandaEMRPOCConfig.MODULE_ID));
+		return l;
+	}
+}
---
+++ api/src/main/java/org/openmrs/module/ugandaemrpoc/UgandaEMRPOCConfig.java
@@ -0,0 +1,14 @@
+package org.openmrs.module.ugandaemrpoc;
+
+import org.springframework.stereotype.Component;
+
+@Component("ugandaemrpoc.UgandaEMRPOCConfig")
+public class UgandaEMRPOCConfig {
+	
+	public static final String MODULE_ID = "ugandaemrpoc";
+	
+	public static final String MODULE_PRIVILEGE = "UgandaEMRPOC Privilege";
+	
+	public UgandaEMRPOCConfig() {
+	}
+}
