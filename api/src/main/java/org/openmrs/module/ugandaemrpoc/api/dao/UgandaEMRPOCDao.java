8e7b51414e39d48dcebe3e95edd1aeb988ecbc90
Stephen Senkomago Musoke <ssmusoke@gmail.com>
2020-11-30 12:46:20 +0300

    Make validation checks for public holidays and maximum patients per day optional (#177)
    * Initial commit of empty working Reference Application Module for UgandaEMR POC
    
    * Add checks on whether the appointment dates can be set on public holidays and the maximum number of patients per day checks are configured and checked

---
+++ api/src/main/java/org/openmrs/module/ugandaemrpoc/api/dao/UgandaEMRPOCDao.java
@@ -0,0 +1,26 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.ugandaemrpoc.api.dao;
+
+import org.openmrs.api.db.hibernate.DbSession;
+import org.openmrs.api.db.hibernate.DbSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("ugandaemrpoc.UgandaEMRPOCDao")
+public class UgandaEMRPOCDao {
+	
+	@Autowired
+	DbSessionFactory sessionFactory;
+	
+	private DbSession getSession() {
+		return sessionFactory.getCurrentSession();
+	}
+}
